cf40bb5fde11fe8dca82fa702a9985bbc89dd40b
Rajan <rajansaini8877@gmail.com>
2025-06-17 13:46:17 +0100

    fix: updated script
    Signed-off-by: Rajan <rajansaini8877@gmail.com>

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,16 +1,17 @@
-console.log("Inside dahboard script");
 
-$("tr.work-item-row").click(function (e) {
-  const workItemNumber = this.childNodes[1].innerHTML.trim();
-  $(`form#work-item-form-${workItemNumber}`).submit();
-});
+$(document).ready(function () {
+  $("tr.work-item-row").click(function (e) {
+    const workItemNumber = this.childNodes[1].innerHTML.trim();
+    $(`form#work-item-form-${workItemNumber}`).submit();
+  });
 
-$("tr.case-row").click(function (e) {
-  const caseNumber = this.childNodes[1].innerHTML.trim();
-  $(`form#case-form-${caseNumber}`).submit();
-});
+  $("tr.case-row").click(function (e) {
+    const caseNumber = this.childNodes[1].innerHTML.trim();
+    $(`form#case-form-${caseNumber}`).submit();
+  });
 
-$("tr.event-row").click(function (e) {
-  const eventNumber = this.childNodes[1].innerHTML.trim();
-  $(`form#event-form-${eventNumber}`).submit();
+  $("tr.event-row").click(function (e) {
+    const eventNumber = this.childNodes[1].innerHTML.trim();
+    $(`form#event-form-${eventNumber}`).submit();
+  });
 });
